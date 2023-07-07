d0bea9a83d57645eb899c085373446e944bcdffc
unknown <camilocr3@hotmail.com>
2023-07-07 07:39:27 -0500

    Ultimo de la noche

--- src/components/TabsGeneral/index.js
+++ src/components/TabsGeneral/index.js
@@ -1,8 +1,31 @@
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+import { Tabs, TabList, TabPanels, Tab, TabPanel, useColorModeValue } from '@chakra-ui/react'
+import { useState } from 'react'
 
 export const TabsGeneral = ({ index, components }) => {
+    
+    const colorsA = (index) =>{
+        let colors = [];
+        for (let i = 0; i < index.length; i++) {
+           colors.push('gray.50')
+        }
+        return colors 
+    }
+    const colorsB = (index) =>{
+        let colors = [];
+        for (let i = 0; i < index.length; i++) {
+           colors.push('gray.900')
+        }
+        return colors 
+    }
+    
+    const colors = useColorModeValue(
+        colorsA(index),
+        colorsB(index),
+      )
+      const [tabIndex, setTabIndex] = useState(0)
+      const bg = colors[tabIndex]
     return (
-        <Tabs variant='soft-rounded' colorScheme='blue'>
+        <Tabs borderRadius={9} onChange={(index) => setTabIndex(index)} bg={bg} variant='soft-rounded' colorScheme='blue'>
             <TabList>
                 {
                     index.map((e, i) => (
